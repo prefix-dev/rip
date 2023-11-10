f7a301f0439340f0135da2304c9ce4b8f5dbd91a
Tim de Jager <tim@prefix.dev>
2023-11-10 11:12:37 +0100

    feat: can read build info from toml

--- crates/rattler_installs_packages/src/sdist.rs
+++ crates/rattler_installs_packages/src/sdist.rs
@@ -32,23 +32,45 @@ impl SDist {
         Self::new(name, Box::new(bytes))
     }
 
-    /// Read .PKG-INFO from the archive
-    pub fn read_package_info(&self) -> miette::Result<(Vec<u8>, WheelCoreMetadata)> {
+    /// Find entry in tar archive
+    fn find_entry(&self, name: impl AsRef<str>) -> miette::Result<Option<Vec<u8>>> {
         let mut archive = self.archive.lock();
-
         // Loop over entries
         for entry in archive.entries().into_diagnostic()? {
             let mut entry = entry.into_diagnostic()?;
 
-            // Find PKG-INFO and parse this file
-            if entry.path().into_diagnostic()?.ends_with("PKG-INFO") {
+            // Find name in archive and return this
+            if entry.path().into_diagnostic()?.ends_with(name.as_ref()) {
                 let mut bytes = Vec::new();
                 entry.read_to_end(&mut bytes).into_diagnostic()?;
-                let metadata = Self::parse_metadata(&bytes)?;
-                return Ok((bytes, metadata));
+                return Ok(Some(bytes));
             }
         }
-        Err(miette!("no PKG-INFO found in archive"))
+        Ok(None)
+    }
+
+    /// Read .PKG-INFO from the archive
+    pub fn read_package_info(&self) -> miette::Result<(Vec<u8>, WheelCoreMetadata)> {
+        if let Some(bytes) = self.find_entry("PKG-INFO")? {
+            let metadata = Self::parse_metadata(&bytes)?;
+            Ok((bytes, metadata))
+        } else {
+            Err(miette!("no PKG-INFO found in archive"))
+        }
+    }
+
+    /// Read the build system info from the pyproject.toml
+    #[allow(dead_code)]
+    pub fn read_build_info(&self) -> miette::Result<pyproject_toml::BuildSystem> {
+        if let Some(bytes) = self.find_entry("pyproject.toml")? {
+            let source = String::from_utf8(bytes).into_diagnostic()?;
+            let project = pyproject_toml::PyProjectToml::new(&source).into_diagnostic()?;
+            Ok(project
+                .build_system
+                .ok_or_else(|| miette!("no build-system found in pyproject.toml"))?)
+        } else {
+            Err(miette!("no pyproject.toml found in archive"))
+        }
     }
 }
 
@@ -100,7 +122,7 @@ impl MetadataArtifact for SDist {
 #[cfg(test)]
 mod tests {
     use crate::MetadataArtifact;
-    use insta::assert_debug_snapshot;
+    use insta::{assert_debug_snapshot, assert_ron_snapshot};
     use std::path::Path;
 
     #[test]
@@ -114,5 +136,27 @@ mod tests {
 
         let metadata = sdist.metadata().unwrap().1;
         assert_debug_snapshot!(metadata);
+    }
+
+    #[test]
+    pub fn read_rich_build_info() {
+        // Read path
+        let path =
+            Path::new(env!("CARGO_MANIFEST_DIR")).join("../../test-data/sdists/rich-13.6.0.tar.gz");
+
+        // Load sdist
+        let sdist = super::SDist::from_path(&path).unwrap();
+
+        let build_system = sdist.read_build_info().unwrap();
+
+        assert_ron_snapshot!(build_system, @r###"
+        BuildSystem(
+          requires: [
+            "poetry-core >=1.0.0",
+          ],
+          r#build-backend: Some("poetry.core.masonry.api"),
+          r#backend-path: None,
+        )
+        "###);
     }
 }
