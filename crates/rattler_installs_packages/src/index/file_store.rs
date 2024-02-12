7bbd73b4456ca4f73a75b97ff11f07511d2ae467
nichmor <nmorkotilo@gmail.com>
2024-02-12 16:37:58 +0200

    misc: format

--- crates/rattler_installs_packages/src/index/file_store.rs
+++ crates/rattler_installs_packages/src/index/file_store.rs
@@ -175,7 +175,10 @@ impl<'a> LockedWriter<'a> {
     /// be used to read from the file again.
     pub fn commit(self) -> io::Result<LockedReader<'a>> {
         self.f.as_file().sync_data().expect("CANT SYNC DATA");
-        let mut file = fs::File::from_parts(self.f.persist(self.path).expect("CANT PERSIS DATA"), self.path);
+        let mut file = fs::File::from_parts(
+            self.f.persist(self.path).expect("CANT PERSIS DATA"),
+            self.path,
+        );
         file.rewind().expect("CANT REWIND DATA");
         Ok(LockedReader {
             file,
